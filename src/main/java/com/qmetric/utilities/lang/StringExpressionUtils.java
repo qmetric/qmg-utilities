2091cb14d20254467c2f937382269ec81a5e1b20
Andrew Eells <aeells@qmetric.co.uk>
2010-10-07 13:39:59 +0100

    Adding StringExpressionUtils.

---
+++ src/main/java/com/qmetric/utilities/lang/StringExpressionUtils.java
@@ -0,0 +1,63 @@
+package com.qmetric.utilities.lang;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * Utils based around the evaluation of string expressions.
+ */
+public final class StringExpressionUtils
+{
+    private static final String VARIABLE_EXPRESSION_START = "{";
+
+    private static final String VARIABLE_EXPRESSION_END = "}";
+
+    private static final String VARIABLE_EXPRESSION_REGEX_START = "\\{";
+
+    private static final String VARIABLE_EXPRESSION_REGEX_END = "\\}";
+
+    /**
+     * Must contain at least one occurrence of "${variable_name}" within expression for it to be classes as dynamic.
+     */
+    private static final Pattern DYNAMIC_EXPRESSION_PATTERN = Pattern.compile("^.*" + VARIABLE_EXPRESSION_REGEX_START + ".+" + VARIABLE_EXPRESSION_REGEX_END + ".*$");
+
+    private StringExpressionUtils()
+    {
+
+    }
+
+    /**
+     * Evaluate a string expression in the following example format "this is a test string with {param_name_1} and {param_name_n}", where "param_name_..." will be resolved based on the
+     * supplied map of parameter names to parameter values.
+     * @param expression The string expression to evaluate.
+     * @param params Map of parameters which include the parameter names defined within the expression.
+     * @return Evaluate string.
+     */
+    public static String evaluateExpression(final String expression, final Map<String, String> params)
+    {
+        String result = expression;
+        if (StringUtils.isNotBlank(expression) && DYNAMIC_EXPRESSION_PATTERN.matcher(expression).find())
+        {
+            final String[] variableNames = StringUtils.substringsBetween(expression, VARIABLE_EXPRESSION_START, VARIABLE_EXPRESSION_END);
+            if (variableNames.length > 0)
+            {
+                for (final String variableName : variableNames)
+                {
+                    if (StringUtils.isNotBlank(variableName))
+                    {
+                        final String value = params.get(variableName);
+
+                        final StringBuilder variableExpression = new StringBuilder();
+                        variableExpression.append(VARIABLE_EXPRESSION_REGEX_START).append(variableName).append(VARIABLE_EXPRESSION_REGEX_END);
+
+                        result = result.replaceAll(variableExpression.toString(), value != null ? value : StringUtils.EMPTY);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/qmetric/utilities/lang/StringExpressionUtilsTest.java
@@ -0,0 +1,72 @@
+package com.qmetric.utilities.lang;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+
+public final class StringExpressionUtilsTest
+{
+    @Test
+    public void handleNullEvaluation()
+    {
+        assertNull("Null expected", StringExpressionUtils.evaluateExpression(null, new HashMap<String, String>()));
+    }
+
+    @Test
+    public void handleEmptyEvaluation()
+    {
+        assertEquals("Unexpected string", "", StringExpressionUtils.evaluateExpression("", new HashMap<String, String>()));
+    }
+
+    @Test
+    public void handleStringEvaluationWithoutDynamicParams()
+    {
+        assertEquals("Unexpected string", "a string to evaluate", StringExpressionUtils.evaluateExpression("a string to evaluate", new HashMap<String, String>()));
+    }
+
+    @Test
+    public void handleStringEvaluationWithDynamicParams()
+    {
+
+        final HashMap<String, String> params = new HashMap<String, String>();
+        params.put("param1", "hello");
+        params.put("param2", "abc");
+
+        assertEquals("Unexpected string", "a hello string to abc evaluate", StringExpressionUtils.evaluateExpression("a {param1} string to {param2} evaluate", params));
+    }
+
+    @Test
+    public void handleUnusedParams()
+    {
+
+        final HashMap<String, String> params = new HashMap<String, String>();
+        params.put("param1", "hello");
+        params.put("param2", "abc");
+        params.put("param3", "abc");
+
+        assertEquals("Unexpected string", "a string to hello evaluate", StringExpressionUtils.evaluateExpression("a string to {param1} evaluate", params));
+    }
+
+    @Test
+    public void handleNullParamValue()
+    {
+
+        final HashMap<String, String> params = new HashMap<String, String>();
+        params.put("param1", null);
+
+        assertEquals("Unexpected string", "a string to  evaluate", StringExpressionUtils.evaluateExpression("a string to {param1} evaluate", params));
+    }
+
+    @Test
+    public void handleBlankParamValue()
+    {
+
+        final HashMap<String, String> params = new HashMap<String, String>();
+        params.put("param1", "");
+
+        assertEquals("Unexpected string", "a string to  evaluate", StringExpressionUtils.evaluateExpression("a string to {param1} evaluate", params));
+    }
+}
