805a17347bb85adc588bf3c4d69a05bfae2b088c
dominicfarr <dominicfarr@gmail.com>
2010-11-05 09:59:35 +0000

    Added new test on uuid, replacement of hypen with (char)0 was causing unicode character not space. This is now fixed with correct regular expression.

--- src/test/java/com/qmetric/utilities/uuid/StrippedUUIDTest.java
+++ src/test/java/com/qmetric/utilities/uuid/StrippedUUIDTest.java
@@ -1,22 +1,48 @@
 package com.qmetric.utilities.uuid;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * User: dfarr Date: Oct 29, 2010 Time: 8:57:31 AM
  */
 public class StrippedUUIDTest
 {
-    @Test
-    public void shouldReturnUUIDWithoutDashes() {
-        assertThat(new StrippedUUID().generate().indexOf('-'), equalTo(-1));
+    private String testUUID;
+
+    @Before
+    public void context()
+    {
+        testUUID = new StrippedUUID().generate();
     }
 
     @Test
-    public void shouldReturnUUIDOf36Characters() {
-        assertThat(new StrippedUUID().generate().length(), equalTo(36));
+    public void shouldReturnUUIDWithoutDashes()
+    {
+        assertThat(testUUID.indexOf('-'), equalTo(-1));
+    }
+
+    @Test
+    public void shouldReturnUUIDOf36Characters()
+    {
+        assertThat(testUUID.length(), equalTo(32));
+    }
+
+    @Test
+    public void shouldOnlyContainValidCharacters()
+    {
+        for (char c : testUUID.toCharArray())
+        {
+            assertTrue("'" + c + "' is not a digit or letter", withinAcceptableRange(c));
+        }
+    }
+
+    private boolean withinAcceptableRange(final char c)
+    {
+        return Character.isLetterOrDigit(c);
     }
 }
