e149673bea522c342a97dc880677217cc77ddc2e
dominicfarr <dominicfarr@gmail.com>
2011-04-15 14:15:36 +0100

    Removed the service annotation from TimeSource

--- src/main/java/com/qmetric/utilities/time/DefaultDateTimeSource.java
+++ src/main/java/com/qmetric/utilities/time/DefaultDateTimeSource.java
@@ -1,9 +1,7 @@
 package com.qmetric.utilities.time;
 
 import org.joda.time.DateTime;
-import org.springframework.stereotype.Service;
 
-@Service
 public class DefaultDateTimeSource implements DateTimeSource
 {
     @Override public DateTime now()
