9e5b2c9304154f6fc5b037c409862a0ff5cf43cb
mburns <mburns@qmetric.co.uk>
2011-05-20 09:23:23 +0100

    re-adding tests that were dropped when pricing package was moved from engine to utils

---
+++ src/test/java/com/qmetric/infrastructure/pricing/MonetaryRoundingTest.java
@@ -0,0 +1,24 @@
+package com.qmetric.infrastructure.pricing;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class MonetaryRoundingTest
+{
+    private final MonetaryRounding monetaryRounding = new MonetaryRounding();
+
+    @Test
+    public void shouldCorrectlyRoundToTwoPlaces()
+    {
+        assertThat(monetaryRounding.round(new BigDecimal("10.499")), equalTo(new BigDecimal("10.50")));
+        assertThat(monetaryRounding.round(new BigDecimal("10.495")), equalTo(new BigDecimal("10.50")));
+        assertThat(monetaryRounding.round(new BigDecimal("10.491")), equalTo(new BigDecimal("10.49")));
+        assertThat(monetaryRounding.round(new BigDecimal("0.00")), equalTo(new BigDecimal("0.00")));
+        assertThat(monetaryRounding.round(new BigDecimal("10.00")), equalTo(new BigDecimal("10.00")));
+    }
+}
+
