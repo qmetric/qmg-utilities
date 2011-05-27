1ac5bdaa8d99316e7e48640768a776620a0eef62
dominicfarr <dominicfarr@gmail.com>
2011-05-27 16:17:10 +0100

    Added new module for tomcat error report valve. Sorted out the dependencies in each module so that test and xml dependencies are correctly packaged.

---
+++ tomcat-error-report-valve/src/main/java/qmetric/tomcat/extension/valve/SimpleErrorReportValve.java
@@ -0,0 +1,123 @@
+package qmetric.tomcat.extension.valve;
+
+import org.apache.catalina.Globals;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.util.RequestUtil;
+import org.apache.catalina.valves.ValveBase;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Custom error report valve for tomcat.
+ * <p/>
+ * Configure in the server.xml by adding errorReportValveClass into the Host element.
+ * <p/>
+ * e.g. <Host appBase="webapps" errorReportValveClass="qmetric.tomcat.extension.valve.SimpleErrorReportValve" ... >
+ * <p/>
+ * See tomcat docs. http://tomcat.apache.org/tomcat-6.0-doc/config/host.html
+ * <p/>
+ * Created: May 27, 2011, Author: Dom Farr
+ */
+
+public final class SimpleErrorReportValve extends ValveBase
+{
+    public void invoke(Request request, Response response) throws IOException, ServletException
+    {
+
+        // Perform the request
+        getNext().invoke(request, response);
+
+        if (response.isCommitted())
+        {
+            return;
+        }
+
+        if (request.getAttribute(Globals.EXCEPTION_ATTR) != null)
+        {
+
+            // The response is an error
+            response.setError();
+
+            // Reset the response (if possible)
+            try
+            {
+                response.reset();
+            }
+            catch (IllegalStateException e)
+            {
+                // can't do any about that
+            }
+
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+        response.setSuspended(false);
+
+        try
+        {
+            report(response);
+        }
+        catch (Throwable t)
+        {
+            // can't do any about that
+        }
+    }
+
+    /**
+     * Prints out an error report.
+     *
+     * @param response The response being generated
+     */
+    private void report(Response response)
+    {
+
+        // Do nothing on a 1xx, 2xx and 3xx status
+        // Do nothing if anything has been written already
+        if (isNotError(response))
+        {
+            return;
+        }
+
+        String message = RequestUtil.filter(response.getMessage());
+
+        if (message == null)
+        {
+            message = "";
+        }
+
+        // Do nothing if there is no report for the specified status code
+        if (sm.getString("http." + response.getStatus(), message) == null)
+        {
+            return;
+        }
+
+        try
+        {
+            response.setContentType("text/html");
+
+            response.setCharacterEncoding("utf-8");
+
+            Writer writer = response.getReporter();
+
+            writer.write(message);
+        }
+        catch (Throwable t)
+        {
+            if (container.getLogger().isDebugEnabled())
+            {
+                container.getLogger().debug("failed to write error report", t);
+            }
+        }
+    }
+
+    private boolean isNotError(final Response response)
+    {
+        return (response.getStatus() < 400) || (response.getContentCount() > 0);
+    }
+}
+
---
+++ tomcat-error-report-valve/src/test/java/qmetric/tomcat/extension/valve/SimpleErrorReportValveTest.java
@@ -0,0 +1,73 @@
+package qmetric.tomcat.extension.valve;
+
+import org.apache.catalina.Globals;
+import org.apache.catalina.Valve;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SimpleErrorReportValveTest
+{
+    private Request request = Mockito.mock(Request.class);
+
+    private Response response = Mockito.mock(Response.class);
+
+    private Valve nextValue = Mockito.mock(Valve.class);
+
+    private SimpleErrorReportValve simpleErrorReportValve;
+
+    @Before
+    public void context()
+    {
+        simpleErrorReportValve = new SimpleErrorReportValve();
+
+        simpleErrorReportValve.setNext(nextValue);
+    }
+
+    @Test
+    public void shouldReturnWithoutReportWhenResponseIsCommitted() throws Exception
+    {
+        when(response.isCommitted()).thenReturn(true);
+
+        simpleErrorReportValve.invoke(request, response);
+
+        verify(response, never()).reset();
+        verify(response, never()).setError();
+        verify(response, never()).setSuspended(false);
+        verify(response, never()).getReporter();
+    }
+
+    @Test
+    public void shouldReturnWithoutReportWhenResponseIsNotError() throws Exception
+    {
+        when(response.isCommitted()).thenReturn(false);
+        when(response.getStatus()).thenReturn(200);
+
+        simpleErrorReportValve.invoke(request, response);
+
+        verify(response, never()).reset();
+        verify(response, never()).setError();
+        verify(response, never()).getReporter();
+    }
+
+    @Test
+    public void shouldCreateSimpleReportWhenResponseIsAnError() throws Exception
+    {
+        when(response.isCommitted()).thenReturn(false);
+        when(request.getAttribute(Globals.EXCEPTION_ATTR)).thenReturn(new Object());
+        when(response.getStatus()).thenReturn(400);
+
+        simpleErrorReportValve.invoke(request, response);
+
+        verify(response).reset();
+        verify(response).setError();
+        verify(response).setSuspended(false);
+        verify(response).getReporter();
+    }
+}
