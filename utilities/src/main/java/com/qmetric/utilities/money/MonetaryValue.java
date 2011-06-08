a1c9904a5b25b30579c3d0c7e10e171e210f3eac
wayne <wbuckley@qmetric.co.uk>
2011-06-08 12:27:16 +0100

    MonetaryValue can be multiplied by integer. MonetaryValue subtract maintains rounding strategy.

--- utilities/src/main/java/com/qmetric/utilities/money/MonetaryValue.java
+++ utilities/src/main/java/com/qmetric/utilities/money/MonetaryValue.java
@@ -61,6 +61,11 @@ public class MonetaryValue implements Comparable<MonetaryValue>, Serializable
         return new MonetaryValue(result, roundingStrategy);
     }
 
+    public MonetaryValue multiply(final int i)
+    {
+        return multiply(new BigDecimal(i));
+    }
+
     private BigDecimal scale(BigDecimal candidate)
     {
         return roundingStrategy.round(candidate);
@@ -80,7 +85,7 @@ public class MonetaryValue implements Comparable<MonetaryValue>, Serializable
     public MonetaryValue subtract(final MonetaryValue subtrahend)
     {
         final BigDecimal result = scale(this.value.subtract(subtrahend.value));
-        return new MonetaryValue(result);
+        return new MonetaryValue(result, roundingStrategy);
     }
 
     @Override
