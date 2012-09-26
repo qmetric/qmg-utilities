b2c8b5813790c26077c0423f96d75bc5bad1d848
tclark <tclark@qmetric.co.uk>
2012-09-26 10:16:55 +0100

    Add utility class for retrieving data from an S3 bucket

---
+++ s3/src/main/java/com/qmetric/utilities/s3/BucketService.java
@@ -0,0 +1,111 @@
+package com.qmetric.utilities.s3;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Optional;
+import com.google.common.io.CharStreams;
+import org.apache.log4j.Logger;
+import org.jets3t.service.S3Service;
+import org.jets3t.service.S3ServiceException;
+import org.jets3t.service.ServiceException;
+import org.jets3t.service.model.S3Bucket;
+import org.jets3t.service.model.S3Object;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Encapsulates storage and retrieval of strings to a single S3 bucket.
+ */
+public class BucketService
+{
+    private final Logger logger = Logger.getLogger(BucketService.class);
+
+    static final String NO_SUCH_KEY = "NoSuchKey";
+
+    private final S3Service s3Service;
+
+    private final S3Bucket bucket;
+
+    public BucketService(final S3Service s3Service, final String bucketName) throws S3ServiceException
+    {
+        this.s3Service = s3Service;
+        this.bucket = s3Service.getBucket(bucketName);
+        if (bucket == null)
+        {
+            throw new IllegalArgumentException(String.format("Bucket called %s cannot be found on Amazon S3", bucketName));
+        }
+    }
+
+    public void store(String key, String data) throws IOException, NoSuchAlgorithmException, S3ServiceException
+    {
+        S3Object stringData = new S3Object(key, data);
+        s3Service.putObject(bucket, stringData);
+    }
+
+    public Optional<String> retrieve(String key)
+    {
+        S3Object s3Object;
+        try
+        {
+            s3Object = s3Service.getObject(bucket.getName(), key);
+        }
+        catch (S3ServiceException e)
+        {
+            if (!NO_SUCH_KEY.equals(e.getS3ErrorCode()))
+            {
+                logError(e);
+            }
+            return Optional.absent();
+        }
+
+        try
+        {
+            String content = getStringFromStream(s3Object.getDataInputStream());
+
+            if (verifyData(s3Object, content.getBytes(Charsets.UTF_8)))
+            {
+                return Optional.of(content);
+            }
+        }
+        catch (IOException e)
+        {
+            logError(e);
+        }
+        catch (ServiceException e)
+        {
+            logError(e);
+        }
+
+        return Optional.absent();
+    }
+
+    private boolean verifyData(S3Object s3Object, byte[] content)
+    {
+        try
+        {
+            return s3Object.verifyData(content);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            logError(e);
+        }
+        catch (IOException e)
+        {
+            logError(e);
+        }
+
+        return false;
+    }
+
+    private String getStringFromStream(InputStream stream) throws IOException
+    {
+        return CharStreams.toString(new InputStreamReader(stream, Charsets.UTF_8));
+    }
+
+    private void logError(Throwable t)
+    {
+        logger.error("Error storing/retrieving data with BucketService", t);
+    }
+}
---
+++ s3/src/test/java/com/qmetric/utilities/s3/BucketServiceTest.java
@@ -0,0 +1,161 @@
+package com.qmetric.utilities.s3;
+
+import com.google.common.base.Optional;
+import org.jets3t.service.S3Service;
+import org.jets3t.service.S3ServiceException;
+import org.jets3t.service.ServiceException;
+import org.jets3t.service.model.S3Bucket;
+import org.jets3t.service.model.S3Object;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class BucketServiceTest
+{
+
+    private final String key = "key";
+
+    private final String data = "data";
+
+    private final S3Service s3Service = mock(S3Service.class);
+
+    private final String bucketName = "bucket";
+
+    private final S3Bucket bucket = mock(S3Bucket.class);
+
+    private BucketService bucketService;
+
+    @Before
+    public void setUp() throws S3ServiceException
+    {
+        when(bucket.getName()).thenReturn(bucketName);
+        when(s3Service.getBucket(bucketName)).thenReturn(bucket);
+        bucketService = new BucketService(s3Service, bucketName);
+    }
+
+    @After
+    public void tearDown()
+    {
+        bucketService = null;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ShouldNotCreateServiceWithInvalidBucket() throws S3ServiceException
+    {
+        when(s3Service.getBucket(bucketName)).thenReturn(null);
+        bucketService = new BucketService(s3Service, bucketName);
+    }
+
+    @Test
+    public void shouldStoreDataInConfiguredBucket() throws IOException, NoSuchAlgorithmException, S3ServiceException
+    {
+        bucketService.store(key, data);
+
+        verify(s3Service).putObject(eq(bucket), any(S3Object.class));
+    }
+
+    @Test
+    public void shouldRetrieveVerifiedContent() throws ServiceException, IOException, NoSuchAlgorithmException
+    {
+        final S3Object s3Object = mock(S3Object.class);
+        when(s3Object.getDataInputStream()).thenReturn(new ByteArrayInputStream(data.getBytes()));
+        when(s3Object.verifyData(data.getBytes())).thenReturn(true);
+
+        when(s3Service.getObject(bucketName, key)).thenReturn(s3Object);
+
+        Optional<String> retrieved = bucketService.retrieve(key);
+        assertTrue(retrieved.isPresent());
+        assertEquals(data, retrieved.get());
+    }
+
+    @Test
+    public void shouldReturnAbsentIfContentUnverified() throws ServiceException, IOException, NoSuchAlgorithmException
+    {
+        final S3Object s3Object = mock(S3Object.class);
+        when(s3Object.getDataInputStream()).thenReturn(new ByteArrayInputStream(data.getBytes()));
+        when(s3Object.verifyData(data.getBytes())).thenReturn(false);
+
+        when(s3Service.getObject(bucketName, key)).thenReturn(s3Object);
+
+        assertFalse(bucketService.retrieve(key).isPresent());
+    }
+
+    @Test
+    public void shouldReturnAbsentIfVerifyThrowsNoSuchAlgorithm() throws ServiceException, IOException, NoSuchAlgorithmException
+    {
+        final S3Object s3Object = mock(S3Object.class);
+        when(s3Object.getDataInputStream()).thenReturn(new ByteArrayInputStream(data.getBytes()));
+        when(s3Object.verifyData(data.getBytes())).thenThrow(new NoSuchAlgorithmException());
+
+        when(s3Service.getObject(bucketName, key)).thenReturn(s3Object);
+
+        assertFalse(bucketService.retrieve(key).isPresent());
+    }
+
+    @Test
+    public void shouldReturnAbsentIfVerifyThrowsIOException() throws ServiceException, IOException, NoSuchAlgorithmException
+    {
+        final S3Object s3Object = mock(S3Object.class);
+        when(s3Object.getDataInputStream()).thenReturn(new ByteArrayInputStream(data.getBytes()));
+        when(s3Object.verifyData(data.getBytes())).thenThrow(new IOException());
+
+        when(s3Service.getObject(bucketName, key)).thenReturn(s3Object);
+
+        assertFalse(bucketService.retrieve(key).isPresent());
+    }
+
+    @Test
+    public void shouldReturnAbsentIfNoKey() throws S3ServiceException
+    {
+        final S3ServiceException s3ServiceException = mock(S3ServiceException.class);
+        when(s3ServiceException.getS3ErrorCode()).thenReturn(BucketService.NO_SUCH_KEY);
+        when(s3Service.getObject(bucketName, key)).thenThrow(s3ServiceException);
+
+        assertFalse(bucketService.retrieve(key).isPresent());
+    }
+
+    @Test
+    public void shouldReturnAbsentIfS3ServiceExceptionThrown() throws S3ServiceException
+    {
+        when(s3Service.getObject(bucketName, key)).thenThrow(new S3ServiceException());
+
+        assertFalse(bucketService.retrieve(key).isPresent());
+    }
+
+    @Test
+    public void shouldReturnAbsentIfServiceExceptionThrown() throws ServiceException
+    {
+        final S3Object s3Object = mock(S3Object.class);
+        when(s3Object.getDataInputStream()).thenThrow(new ServiceException());
+        when(s3Service.getObject(bucketName, key)).thenReturn(s3Object);
+
+        assertFalse(bucketService.retrieve(key).isPresent());
+    }
+
+    @Test
+    public void shouldReturnAbsentIfIOExceptionThrown() throws ServiceException
+    {
+        final InputStream inputStream = mock(InputStream.class);
+
+        final S3Object s3Object = mock(S3Object.class);
+        when(s3Object.getDataInputStream()).thenReturn(inputStream);
+
+        when(s3Service.getObject(bucketName, key)).thenReturn(s3Object);
+
+        assertFalse(bucketService.retrieve(key).isPresent());
+    }
+}
